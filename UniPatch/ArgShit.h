adfd88098c4182548fce59917ec94aeb985f0563
LeontKing2 <84083235+LeontKing2@users.noreply.github.com>
2024-02-07 15:00:15 +0530

    Update ArgShit.h

--- UniPatch/ArgShit.h
+++ UniPatch/ArgShit.h
@@ -3,32 +3,31 @@
 #include <sstream>
 #include <codecvt>
 
-std::wstring to_wstring(std::string str);
-std::string to_string(std::wstring str);
+std::wstring to_wstring(const std::string& str);
+std::string to_string(const std::wstring& str);
 std::string leadingZero(uint64_t num);
 
 class ArgShit {
 private:
-	char** argv;
-	int argc;
-	int i;
-	std::wstring s;
+    char** argv;
+    int argc;
+    int i;
+    std::wstring s;
 
 public:
-	ArgShit();
+    ArgShit();
 
-	ArgShit(char* _argv[], int _argc);
+    ArgShit(char* _argv[], int _argc);
 
-	ArgShit(char* _argv[], int _argc, const char* find);
+    ArgShit(char* _argv[], int _argc, const char* find);
 
-	void parseArg(const char* find);
+    void parseArg(const char* find);
 
-	char* getArg(int ind);
+    char* getArg(int ind);
 
-	bool contains(const char* test);
+    bool contains(const char* test) const;
 
-	int getInt();
+    int getInt();
 
-	std::wstring getString();
-
+    std::wstring getString();
 };
